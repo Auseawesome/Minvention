e83012e6d080746a248f9936b6842fb9df4c7354
Auseawesome <ajthomasbtb@outlook.com>
2023-11-27 16:23:49 +0000

    Fixed mechanical drill display

--- scripts/override.js
+++ scripts/override.js
@@ -12,4 +12,5 @@ Events.on(ClientLoadEvent, e => {
     drillRequireLiquid(Blocks.laserDrill, [new ConsumeLiquid(Liquids.water, 0.08)])
     drillRequireLiquid(Blocks.blastDrill, [new ConsumeLiquid(Liquids.water, 0.1)])
     Blocks.mechanicalDrill.liquidCapacity = 0
+    Blocks.mechanicalDrill.hasLiquids = false
 })
