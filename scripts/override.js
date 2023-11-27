e77a635dd14d96492e8faeb65bb16f744bc1f11f
Auseawesome <ajthomasbtb@outlook.com>
2023-11-27 16:15:34 +0000

    Apply to all drills

--- scripts/override.js
+++ scripts/override.js
@@ -1,6 +1,14 @@
-Events.on(ClientLoadEvent, e => {
-    let drill = Blocks.pneumaticDrill;
+function drillRequireLiquid(drill,liquids) {
     drill.liquidBoostIntensity = 1;
-    drill.consumers = drill.nonOptionalConsumers = [new ConsumeLiquid(Liquids.water, 0.2)];
+    drill.consumers = drill.nonOptionalConsumers = liquids;
     drill.optionalConsumers = [];
+}
+
+
+
+Events.on(ClientLoadEvent, e => {
+    drillRequireLiquid(Blocks.mechanicalDrill, [])
+    drillRequireLiquid(Blocks.pneumaticDrill, [new ConsumeLiquid(Liquids.water, 0.06)])
+    drillRequireLiquid(Blocks.laserDrill, [new ConsumeLiquid(Liquids.water, 0.08)])
+    drillRequireLiquid(Blocks.airblastDrill, [new ConsumeLiquid(Liquids.water, 0.1)])
 })
