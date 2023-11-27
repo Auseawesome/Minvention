79b6beceaed241c8ec1d0eebf571b2d1bb468829
Auseawesome <ajthomasbtb@outlook.com>
2023-11-27 07:50:27 +0000

    Going back to suggested code on discord

--- scripts/override.js
+++ scripts/override.js
@@ -1,4 +1,4 @@
 let drill = Blocks.pneumaticDrill;
 drill.liquidBoostIntensity = 1;
-drill.consumes = [];
-drill.consumes = [new ConsumeLiquid(Liquids.water, 0.2).optional(false,false)]
+drill.consumers = drill.nonOptionalConsumers = [new ConsumeLiquid(Liquids.water, 0.2).optional(false,false)];
+drill.optionalConsumers = [];
