7d3a2a293f9956bd91efdd5c0606a66b58dbaa1d
Auseawesome <ajthomasbtb@outlook.com>
2023-11-27 07:38:54 +0000

    Hopefully fixed

--- scripts/override.js
+++ scripts/override.js
@@ -1,3 +1,4 @@
 let drill = Blocks.pneumaticDrill;
+drill.liquidBoostIntensity = 1;
 drill.consumers = drill.nonOptionalConsumers = [new ConsumeLiquid(Liquids.water, 0.2)];
 drill.optionalConsumers = [];
