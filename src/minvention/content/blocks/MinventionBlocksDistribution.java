a6304657a21a2dc93228a5a2d98fe574ceb73863
Auseawesome <ajthomasbtb@outlook.com>
2025-03-27 21:35:53 +0000

    Added Mechanical Conveyor

---
+++ src/minvention/content/blocks/MinventionBlocksDistribution.java
@@ -0,0 +1,29 @@
+package minvention.content.blocks;
+
+import mindustry.content.Items;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.distribution.*;
+import minvention.content.MinventionBlocks;
+
+import static mindustry.type.ItemStack.*;
+
+public class MinventionBlocksDistribution {
+    public static Block
+    // Conveyors
+    mechanicalConveyor
+
+    ;
+
+    public static void load(){
+        mechanicalConveyor = new Conveyor("mechanical-conveyor") {{
+            requirements(Category.distribution, with(Items.copper,1));
+            health = 45;
+            speed = 0.03f;
+            displayedSpeed = 4.2f;
+            buildCostMultiplier = 2f;
+            researchCost = with(Items.copper, 5);
+        }};
+        MinventionBlocks.ivanorBlocks.addAll(new Block[]{mechanicalConveyor});
+    }
+}
