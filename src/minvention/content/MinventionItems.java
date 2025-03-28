294af4b8e957919ada56ed18e850c18932fe1c28
Auseawesome <ajthomasbtb@outlook.com>
2025-03-28 14:58:45 +0000

    Added Iron

--- src/minvention/content/MinventionItems.java
+++ src/minvention/content/MinventionItems.java
@@ -7,10 +7,14 @@ import mindustry.type.Item;
 
 public class MinventionItems {
 	public static Item
-	// Natural
-	snow, ice;
+			// Metals
+			iron,
+
+			// Natural
+			snow, ice;
     
     public static void load() {
+		iron = new Item("item-iron", Color.valueOf("888faa"));
 		snow = new Item("item-snow", Color.valueOf("e1e9f0"));
         ice = new Item("item-ice", Color.valueOf("c2bffb"));
 	}
