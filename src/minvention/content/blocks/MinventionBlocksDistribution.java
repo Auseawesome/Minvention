d509a5b8bdff4b8e6b4bdc90a32f7e151aab4cf3
Auseawesome <ajthomasbtb@outlook.com>
2025-03-28 17:02:23 +0000

    Changed distribution item requirements

--- src/minvention/content/blocks/MinventionBlocksDistribution.java
+++ src/minvention/content/blocks/MinventionBlocksDistribution.java
@@ -1,11 +1,13 @@
 package minvention.content.blocks;
 
-import mindustry.content.Items;
+import mindustry.content.Blocks;
 import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.blocks.distribution.*;
 import minvention.content.MinventionBlocks;
 
+import static mindustry.content.Items.*;
+import static minvention.content.MinventionItems.*;
 import static mindustry.type.ItemStack.*;
 
 public class MinventionBlocksDistribution {
@@ -16,6 +18,12 @@ public class MinventionBlocksDistribution {
     ;
 
     public static void load(){
-
+        Blocks.conveyor.requirements(Category.distribution, with(
+                iron, 1
+        ));
+        Blocks.router.requirements(Category.distribution, with(
+                iron, 2,
+                copper, 1
+        ));
     }
 }
