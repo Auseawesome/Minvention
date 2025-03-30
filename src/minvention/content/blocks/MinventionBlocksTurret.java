873134146128ffc961d9a6a7cc4fc2c3550d1e6b
Auseawesome <ajthomasbtb@outlook.com>
2025-03-30 15:46:54 +0100

    Modified duo ammo

--- src/minvention/content/blocks/MinventionBlocksTurret.java
+++ src/minvention/content/blocks/MinventionBlocksTurret.java
@@ -1,18 +1,49 @@
 package minvention.content.blocks;
 
+import arc.graphics.Color;
 import mindustry.content.Blocks;
+import mindustry.content.Items;
+import mindustry.content.StatusEffects;
+import mindustry.entities.bullet.BasicBulletType;
 import mindustry.type.Category;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import minvention.content.MinventionItems;
 
 import static mindustry.content.Items.*;
 import static minvention.content.MinventionItems.*;
 import static mindustry.type.ItemStack.*;
 
 public class MinventionBlocksTurret {
+    public static void modifyTurret(Block turret_block, Object... objects) {
+        if (turret_block instanceof ItemTurret turret) {
+            turret.ammo(objects);
+        }
+    }
+
     public static void load() {
         Blocks.duo.requirements(Category.turret, with(
                 iron, 30,
                 copper, 5
         ));
+        modifyTurret(Blocks.duo,
+                MinventionItems.iron, new BasicBulletType(2.5f, 9){{
+                    width = 7f;
+                    height = 9f;
+                    lifetime = 60f;
+                    ammoMultiplier = 2;
+                }},
+                MinventionItems.snow, new BasicBulletType(1.0f, 0) {{
+                    width = 7f;
+                    height = 7f;
+                    lifetime = 150f;
+                    ammoMultiplier = 1;
+                    status = StatusEffects.slow;
+                    statusDuration = 1.0f;
+                    backColor = Color.valueOf("d2e6f5");
+                    frontColor = Color.valueOf("f3f5ff");
+                }}
+        );
         Blocks.scatter.requirements(Category.turret, with(
                 iron, 100,
                 copper, 30
