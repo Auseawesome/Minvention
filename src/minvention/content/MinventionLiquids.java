c5ec6ca12fb61c37e4069f78bbdf02f0109c127f
Auseawesome <ajthomasbtb@outlook.com>
2025-03-31 10:57:11 +0100

    Updated steam heat capacity and temperature

--- src/minvention/content/MinventionLiquids.java
+++ src/minvention/content/MinventionLiquids.java
@@ -1,8 +1,11 @@
 package minvention.content;
 
+import mindustry.content.Liquids;
 import minvention.audiovisual.MinventionPal;
 import mindustry.content.StatusEffects;
 import mindustry.type.Liquid;
+
+import static minvention.MinventionVars.*;
 
 public class MinventionLiquids {
     public static Liquid
@@ -12,9 +15,8 @@ public class MinventionLiquids {
     public static void load() {
         steam = new Liquid("gas-steam", MinventionPal.steamFront) {{
             gas = true;
-            alwaysUnlocked = true;
-            explosiveness = 0f;
-            temperature = 0.6f;
+            heatCapacity = Liquids.water.heatCapacity * waterPerSteam;
+            temperature = 0.8f;
             effect = StatusEffects.wet;
         }};
     }
