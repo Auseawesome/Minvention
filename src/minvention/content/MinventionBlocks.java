7fcc652e45a317846a6c7fa6230add41ad8fc479
Auseawesome <ajthomasbtb@outlook.com>
2025-04-03 11:02:16 +0100

    Added new automatic block requirement conversion

--- src/minvention/content/MinventionBlocks.java
+++ src/minvention/content/MinventionBlocks.java
@@ -1,10 +1,29 @@
 package minvention.content;
 
-import arc.struct.Seq;
+import mindustry.content.Items;
+import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import minvention.content.blocks.*;
 
 public class MinventionBlocks {
+    // Performs a basic item to item conversion to new replacement items
+    // Only use for quick compatibility, not ideal
+    public static ItemStack[] convertItems(ItemStack[] items) {
+        for (ItemStack itemStack : items) {
+            if (itemStack.item == Items.copper) {
+                itemStack.item = MinventionItems.iron;
+            } else if (itemStack.item == Items.graphite) {
+                itemStack.item = MinventionItems.refinedCarbon;
+            }
+        }
+        return items;
+    }
+
+    // Performs a basic item to item conversion to hopefully make a block properly obtainable in the new campaign
+    // Only use for quick compatibility, not ideal
+    public static void convertRequirements(Block block) {
+        block.requirements = convertItems(block.requirements);
+    };
 
     public static void load() {
         MinventionBlocksProduction.load();
